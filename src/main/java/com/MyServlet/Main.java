18c609b43d3d5868b566b6a6ffb9c8745d24113e
injecti0n <rootgebruiker@gmail.com>
2019-01-07 15:36:26 +0100

    first commit

--- src/main/java/com/MyServlet/Main.java
+++ src/main/java/com/MyServlet/Main.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/getParameters")
-public class ServletUrlParameterExample extends HttpServlet {
+public class Main extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
